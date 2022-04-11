44dad8765f7897511a572df265a864a1f62b4f9b
LoomisM4 <mail@marcel-wettach.com>
2022-04-11 16:19:51 +0200

    * .gitignore: initial commit with some changes
    * mt-shop-cc.sln:
    * App.xaml:
    * Main.cs:
    * App.xaml.cs:
    * AppShell.xaml:
    * Info.plist:
    * Spotlight.xaml:
    * AssemblyInfo.cs:
    * AppShell.xaml.cs:
    * mt-shop-cc.csproj:
    * Spotlight.xaml.cs:
    * AppDelegate.cs:
    * GettingStarted.txt:
    * Entitlements.plist:
    * MainActivity.cs:
    * Default.png:
    * mt-shop-cc.iOS.csproj:
    * icon_feed.png:
    * icon_about.png:
    * Default@2x.png:
    * icon_feed@3x.png:
    * icon_feed@2x.png:
    * AssemblyInfo.cs:
    * xamarin_logo.png:
    * AboutAssets.txt:
    * icon_about@2x.png:
    * icon_about@3x.png:
    * mt-shop-cc.Android.csproj:
    * xamarin_logo@2x.png:
    * xamarin_logo@3x.png:
    * Default-568h@2x.png:
    * Default-Portrait.png:
    * AssemblyInfo.cs:
    * styles.xml:
    * colors.xml:
    * AboutResources.txt:
    * LaunchScreen.storyboard:
    * Default-Portrait@2x.png:
    * Resource.designer.cs:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon_feed.png:
    * icon.png:
    * icon.png:
    * icon_about.png:
    * xamarin_logo.png:
    * icon.xml:
    * Icon29.png:
    * Icon40.png:
    * Icon60.png:
    * Icon20.png:
    * Icon58.png:
    * Icon80.png:
    * Icon87.png:
    * Icon76.png:
    * Icon180.png:
    * icon_round.xml:
    * Icon120.png:
    * Icon152.png:
    * Icon167.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * mt-shop-cc.Android.csproj.AssemblyReference.cache:
    * mt-shop-cc.iOS.csproj.AssemblyReference.cache:

---
+++ mt-shop-cc/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace mt_shop_cc
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            //Routing.RegisterRoute(nameof(Spotlight), typeof(Spotlight));
+        }
+
+    }
+}
