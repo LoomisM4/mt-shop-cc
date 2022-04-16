8d27fbf8ba02e25306d9622c9d16f251af782b1d
LoomisM4 <mail@marcel-wettach.com>
2022-04-16 10:43:59 +0200

    article list implemented as prototype

---
+++ mt-shop-cc/ViewModels/ArticleListModel.cs
@@ -0,0 +1,11 @@
+using QuickType;
+using Xamarin.Forms;
+
+namespace mt_shop_cc.ViewModels
+{
+    public class ArticleListModel
+    {
+        public Article Article { get; set; }
+        public ImageSource Image { get; set; }
+    }
+}
--- mt-shop-cc/Views/ArticleList.xaml.cs
+++ mt-shop-cc/Views/ArticleList.xaml.cs
@@ -1,44 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
-
+using mt_shop_cc.ViewModels;
+using QuickType;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace mtshopcc.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ArticleList : ContentPage
+    public partial class ArticleList : ContentPage, IQueryAttributable
     {
-        public ObservableCollection<string> Items { get; set; }
+        public ObservableCollection<ArticleListModel> Items { get; set; }
+        
+        private Uri Url { get; set; }
 
         public ArticleList()
         {
             InitializeComponent();
 
-            Items = new ObservableCollection<string>
-            {
-                "Item 1",
-                "Item 2",
-                "Item 3",
-                "Item 4",
-                "Item 5"
-            };
+            Items = new ObservableCollection<ArticleListModel>();
 
-            MyListView.ItemsSource = Items;
+            ArticlesList.ItemsSource = Items;
+        }
+        
+        public void ApplyQueryAttributes(IDictionary<string, string> query)
+        {
+            Url = new Uri(query["url"]);
+            Title = query["title"];
         }
 
         async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
         {
-            if (e.Item == null)
-                return;
+            var model = (ArticleListModel) e.Item;
+            if (model.Article.Links.Details != null)
+            {
+                await Shell.Current.GoToAsync($"details?url={model.Article.Links.Details.Href}&title={model.Article.Name}");
+            }
+        }
 
-            await DisplayAlert("Item Tapped", "An item was tapped.", "OK");
-
-            //Deselect Item
-            ((ListView)sender).SelectedItem = null;
+        protected override void OnAppearing()
+        {
+            Items.Clear();
+            var task = Api.Articles(Url);
+            task.ContinueWith(r =>
+            {
+                Article[] articles = r.Result;
+                foreach (var a in articles)
+                {
+                    ArticleListModel model = new ArticleListModel();
+                    model.Article = a;
+                    if (a.Links.Preview != null)
+                    {
+                        model.Image = Api.Img(a.Links.Preview.Href);
+                    }
+                    Items.Add(model);
+                }
+            });
+            base.OnAppearing();
         }
     }
 }
